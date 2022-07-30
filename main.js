aa2733758d8edd9b5edd01ce6b10eab657282fb7
IrynaRybka <irarybka@gmail.com>
2022-07-30 13:30:41 +0300

    current city

--- main.js
+++ main.js
@@ -74,5 +74,42 @@ function changeCity(event) {
     alert(`Please, type a city`);
   }
 }
-let myCity = document.querySelector("#seach-form");
+let myCity = document.querySelector("#search-form");
 myCity.addEventListener("submit", changeCity);
+
+// current weather in a submit city
+let apiKey = "ec73684ab43da8e0668f04ae9704e6d3";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+function showTemperature(response) {
+  let temperature = Math.round(response.main.data.temp);
+  let temperatureElement = document.querySelector("#number-temperature");
+  let description = document.querySelector("#forecoast");
+
+  temperatureElement.innerHTML = `${temperature}`;
+  description.innerHTML = response.data.weather[0].description;
+}
+
+axios.get(`${apiUrl}&appid=${apiKey}`).then(showTemperature);
+changeCity(showTemperature);
+
+// current position
+function currentTemperature(response) {
+  let h1 = document.querySelector("h1");
+  let temperature = Math.round(response.data.main.temp);
+  h1.innerHTML = `${response.data.name}`;
+  let description = document.querySelector("#number-temperature");
+  description.innerHTML = `${temperature}`;
+}
+function retrievePosition(position) {
+  let apiKey = "ec73684ab43da8e0668f04ae9704e6d3";
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
+  axios.get(url).then(showWeather);
+}
+
+navigator.geolocation.getCurrentPosition(currentTemperature&&retrievePosition);
+
+let button = document.querySelector("#current-btn");
+button.addEventListener("click", getCurrentPosition);
